c5849572191cebe5ae581138c33a4abff2bc6463
John Jakobsen <jerikjakobsen@gmail.com>
2022-12-05 00:41:33 -0500

    Finished the neural network !

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -7,18 +7,20 @@ class NeuralNetwork:
     # Where x is the number of nodes in each layer 
     # And y is the number of layers
     def __init__(self, inputSize, hiddenLayersSize, outputSize, activation) -> None:
-        hiddenNodeCount = hiddenLayersSize[0]
+        self.hiddenNodeCount = hiddenLayersSize[0]
+        self.lastLayerIndex = hiddenLayersSize[1] + 1
+        self.numOfLayers = hiddenLayersSize[1] + 2
+        self.weightGradient = []
         self.weights = []
-        self.weights.append(rand(hiddenNodeCount, inputSize)) # Set up weight Matrix for input layer to first hidden layer
-        self.weights.extend([rand(hiddenNodeCount, hiddenNodeCount) for i in range(hiddenNodeCount-2)]) # Set up weight matrices for hidden layers
-        self.weights.append(rand(outputSize, hiddenNodeCount)) # Set up Weight Matrix for output layer
+        self.weights.append(rand(self.hiddenNodeCount, inputSize)) # Set up weight Matrix for input layer to first hidden layer
+        self.weights.extend([rand(self.hiddenNodeCount, self.hiddenNodeCount) for i in range(hiddenLayersSize[1]-1)]) # Set up weight matrices for hidden layers
+        self.weights.append(rand(outputSize, self.hiddenNodeCount)) # Set up Weight Matrix for output layer
         self.inputSize = inputSize
-        self.hiddenLayersSize = hiddenLayersSize
         self.outputSize = outputSize
         self.activation = activation
         self.outputs = []
         self.outputs.append(np.zeros((inputSize,1))) # Set up output vector first layer to second layer
-        self.outputs.extend([np.zeros((hiddenNodeCount, 1)) for i in range(hiddenNodeCount-1)]) # Set up output layers for hidden layers
+        self.outputs.extend([np.zeros((self.hiddenNodeCount, 1)) for i in range(self.numOfLayers-2)]) # Set up output layers for hidden layers
         self.outputs.append(np.zeros((outputSize, 1))) # Set up vector for the output layer
 
     # Input should be the same size as the inputNodes provided in initialization
@@ -31,7 +33,7 @@ class NeuralNetwork:
         if len(input) != self.inputSize:
             print("Error: Input provided does not match input layer size")
             return
-        for layer in range(self.hiddenLayersSize[1] + 2):
+        for layer in range(self.numOfLayers):
             if layer == 0:
                 self.outputs[0] = np.array(input).transpose()
             else:
@@ -39,43 +41,54 @@ class NeuralNetwork:
 
     def _trainFromOneSample(self, sampleX, sampleY, trainSize):
         self.predict(sampleX)
-        hiddenNodeCount = self.hiddenLayersSize[0]
-        inputSize = self.inputSize
-        outputSize = self.outputSize
-        self.weightGradient = []
-        self.weightGradient.append(np.zeros((hiddenNodeCount, inputSize))) # Set up weight Matrix for input layer to first hidden layer
-        self.weightGradient.extend([np.zeros((hiddenNodeCount, hiddenNodeCount)) for i in range(hiddenNodeCount-2)]) # Set up weight matrices for hidden layers
-        self.weightGradient.append(np.zeros((outputSize, hiddenNodeCount))) # Set up Weight Matrix for output layer
         AOEInitial = []
-        for i in range(len(self.outputs[self.hiddenLayersSize[1]+1])):
-            nodeOutput = self.outputs[-1][i]
+        for i in range(self.outputSize):
+            nodeOutput = self.outputs[self.lastLayerIndex][i]
             err = - (sampleY[i] - nodeOutput)
             AOEInitial.append(err)
-        self._BackProp(AOEInitial, self.hiddenLayersSize[1] + 1, trainSize)
+        self._BackProp(AOEInitial, self.lastLayerIndex, trainSize)
+
+    def train(self, xSamples, ySamples):
+        self.weightGradient = []
+        self.weightGradient.append(np.zeros((self.hiddenNodeCount, self.inputSize))) # Set up weight Matrix for input layer to first hidden layer
+        self.weightGradient.extend([np.zeros((self.hiddenNodeCount, self.hiddenNodeCount)) for i in range(self.numOfLayers-3)]) # Set up weight matrices for hidden layers
+        self.weightGradient.append(np.zeros((self.outputSize, self.hiddenNodeCount))) # Set up Weight Matrix for output layer
+        if len(xSamples) != len(ySamples):
+            print("Length of X does not match length of Y")
+
+        for sampleIndex in range(len(xSamples)):
+            xSample = xSamples[sampleIndex]
+            ySample = ySamples[sampleIndex]
+            self._trainFromOneSample(xSample, ySample, len(xSample))
+
+        for i in range(len(self.weights)):
+            self.weights[i] -= self.weightGradient[i]
+
 
     def _BackProp(self, AOE, layer, trainSize):
         if layer == 0:
             return
         Deltas = []
-        for j in range(len(AOE)):
-            affectInErr = AOE[j]
-            outForNodeJ = self.outputs[layer][j]
-            delta = affectInErr * outForNodeJ * (1-outForNodeJ)
+        for i in range(len(AOE)):
+            affectInErr = AOE[i]
+            outForNodeI = self.outputs[layer][i]
+            delta = affectInErr * outForNodeI * (1-outForNodeI)
             Deltas.append(delta)
-        
+
         AOENext = np.zeros(self.layerSize(layer-1))
         for j in range(self.layerSize(layer-1)):
             for i in range(self.layerSize(layer)):
-                weightAdjustment = self.outputs[layer-1][j] * Deltas[j]
+                weightAdjustment = self.outputs[layer-1][j] * Deltas[i]
                 self.weightGradient[layer-1][i, j] += weightAdjustment / trainSize
-                AOENext[j] += Deltas[j] * self.weights[layer-1][ i, j]
+                AOENext[j] += Deltas[i] * self.weights[layer-1][i, j]
         self._BackProp(AOENext, layer-1, trainSize)
+
 
 
     def layerSize(self, layer):
         if layer == 0:
             return self.inputSize
-        elif layer == self.hiddenLayersSize[1] + 1:
+        elif layer == self.lastLayerIndex:
             return self.outputSize
         else:
-            return self.hiddenLayersSize[0]
+            return self.hiddenNodeCount
--- testing.py
+++ testing.py
@@ -5,12 +5,16 @@ def sigmoid(x):
     return 1/(1+np.power(np.e, -x))
 def x(x):
     return x
-nn = NeuralNetwork(2, (2, 1), 2, sigmoid)
-nn.predict([10, 4])
+nn = NeuralNetwork(3, (5, 1), 3, sigmoid)
+nn.predict(np.array([7., 4., 2.]))
 print(nn.outputs)
-print(nn.weights)
 
-
-nn._trainFromOneSample(np.array([7., 4.]), np.array([[1.],
-       [0.]]), 1)
-print(nn.weightGradient)
+for i in range(5000):
+    nn.train([np.array([7., 4., 2.]), np.array([2., 9., 6.]),np.array([0., 2., 10.])], [np.array([[1.],
+        [0.], [0.]]),np.array([[0.],
+        [1.], [0.]]),np.array([[0.],
+        [0.], [1.]])])
+# print("weights", nn.weights)
+# print("WEight gradients", nn.weightGradient)
+nn.predict(np.array([2., 9., 6.]))
+print(nn.outputs)
